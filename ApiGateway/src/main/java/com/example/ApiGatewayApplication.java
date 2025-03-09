5bdaaebb9ac63cccf4129446dcda69e181102bbc
Surya Vemuri <156511852+suryavemuri30@users.noreply.github.com>
2025-03-09 11:36:45 +0530

    update

--- ApiGateway/src/main/java/com/example/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -1,15 +1,13 @@
-package com.example.product;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-
-public class ProductServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
